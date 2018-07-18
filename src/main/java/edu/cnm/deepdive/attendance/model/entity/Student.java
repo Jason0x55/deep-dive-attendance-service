06316354bb5d9bc5ec4d30955b537085e1d54573
Jason Rafferty <Jason0x55@gmail.com>
2018-07-18 16:06:07 -0600

    More in class code.

--- src/main/java/edu/cnm/deepdive/attendance/model/entity/Student.java
+++ src/main/java/edu/cnm/deepdive/attendance/model/entity/Student.java
@@ -5,21 +5,28 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import java.net.URI;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 import javax.annotation.PostConstruct;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.Formula;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.EntityLinks;
 import org.springframework.stereotype.Component;
 
 @JsonInclude(Include.NON_NULL)
-@JsonIgnoreProperties(value = {"id", "created"}, allowGetters = true, ignoreUnknown = true)
+@JsonIgnoreProperties(value = {"id", "created", "absences"}, allowGetters = true, ignoreUnknown = true)
 @Component
 @Entity
 public class Student {
@@ -55,12 +62,30 @@ public class Student {
   @Column(length = 20)
   private String phone;
 
+  @OneToMany(fetch = FetchType.LAZY, mappedBy = "student", cascade = CascadeType.ALL)
+  @OrderBy("start DESC")
+  private List<Absence> absences = new LinkedList<>();
+
+  public URI getHref() {
+    return entityLinks.linkForSingleResource(Student.class, id).toUri();
+  }
+
+  @Formula("(SELECT COUNT(*) FROM absence AS ab WHERE ab.student_id = student_id)")
+  private int totalAbsences;
+
+  @Formula("(SELECT COUNT(*) FROM absence AS ab WHERE ab.student_id = student_id AND NOT ab.excused)")
+  private int unexcusedAbsences;
+
   public long getId() {
     return id;
   }
 
   public Date getCreated() {
     return created;
+  }
+
+  public List<Absence> getAbsences() {
+    return absences;
   }
 
   public String getLastName() {
@@ -87,8 +112,12 @@ public class Student {
     this.phone = phone;
   }
 
-  public URI getHref() {
-    return entityLinks.linkFor(Student.class, id).toUri();
+  public int getTotalAbsences() {
+    return totalAbsences;
+  }
+
+  public int getUnexcusedAbsences() {
+    return unexcusedAbsences;
   }
 
   public void patch(Student update) {
